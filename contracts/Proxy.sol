bd09a930220e38e5c66d0cc5467ba847a49e7e5f
Seshadri Rao <bssrinivasarao@gmail.com>
2018-03-08 14:45:35 +0530

    contracts

---
+++ contracts/Proxy.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.18;
+
+import './interfaces/IERC20.sol';
+import './interfaces/IProxy.sol';
+
+contract Proxy is IProxy {
+
+    IERC20 public BT;
+    IERC20 public QT;
+
+    address public option;
+    address public buyer;
+    uint256 public optionsExpiry;
+    uint256 public strikePrice;
+
+    modifier onlyOption() {
+        require(msg.sender == option);
+        _;
+    }
+
+    /**
+     * @dev Constructor
+     * @param _baseToken Address of the Base token
+     * @param _quoteToken Address of the Quote token
+     * @param _expiry Unix timestamp to expire the option
+     * @param _strikePrice Price at which buyer will obligate to buy the base token
+     * @param _buyer Address of the buyer
+     */
+    function Proxy(address _baseToken, address _quoteToken, uint256 _expiry, uint256 _strikePrice, address _buyer) public {
+        option = msg.sender;
+        BT = IERC20(_baseToken);
+        QT = IERC20(_quoteToken); 
+        optionsExpiry = _expiry;
+        strikePrice = _strikePrice;
+        buyer = _buyer;
+    }
+
+    /**
+     * @dev `distributeStakes` Use to settle down the excersice request of the option
+     * @param _to Address of the seller
+     * @param _amount Number of the assets seller want to excercised
+     * @return bool success
+     */
+    function distributeStakes(address _to, uint256 _amount) onlyOption public returns (bool success) {
+        require(QT.transfer(_to, _amount * strikePrice));
+        require(BT.transferFrom(_to, buyer, _amount));
+        return true; 
+    } 
+
+    /**
+     * @dev withdraw the unused base token and quote token only by owner
+     * @return bool success    
+     */
+    function withdrawal() onlyOption public returns (bool success) {
+        require(block.number > optionsExpiry);
+        uint256 balanceBT = BT.balanceOf(this);
+        uint256 balanceQT = QT.balanceOf(this);
+        require(BT.transfer(buyer, balanceBT));
+        require(QT.transfer(buyer, balanceQT));
+        return true;
+    }
+
+}
---
+++ contracts/interfaces/IDerivativeFactory.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.18;
+
+interface IDerivativeFactory {
+
+// function to create the options
+    function createNewOption(address _baseToken, address _quoteToken, uint256 _strikePrice, uint256 _blockNoExpiry) 
+    external 
+    returns (bool); 
+
+}
