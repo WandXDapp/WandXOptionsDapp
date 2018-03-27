0de2ccf9a129dac79ede067dac13b93c6944201d
Nishant Bhatia <nishant@roshi.in>
2018-03-27 11:40:49 +0530

    added backend for user setup

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ DATE        PROGRAMMER      COMMENT
 var apiStrings = require('../config/apiStrings');
 var express = require('express');
 var router = express.Router();
+var db = require('../bin/db');
 
 import { logger } from '../config/logger';
 
@@ -20,9 +21,90 @@ Method: GET
 Purpose: This is the home page of the website. Before login. If user is already authenticated
 		 then user is redirected to the exchange page.
 **/
-router.get('/', function(req, res, next) {	
-	return res.render('index', { 
+router.get('/insertAddress', function(req, res, next) {	
+	
+	var users = db.get().collection('users');
+	var address = req.get("address");
+	users.findOne({ address: address}, function (err, user) {
+		if (err) {
+			return res.status(200).json({
+				status: 'error',
+				error: 'Unable to find address in db'
+			});
+		}
+		
+		if (user !== null) {
+			return res.status(200).json({
+				status: 'error',
+				error: 'Address already exist'
+			});
+		}
+		
+			
+		var user = {
+			address: address
+		};
+		
+		users.insert(user, function (err) {
+			if (err) {
+				return res.status(200).json({
+					status: 'error',
+					error: 'Unable to insert address'
+				});
+			}
+			
+			return res.status(200).json({
+				status: 'ok',
+				error: ''
+			});
+		});
+	});
+});
 
+router.get('/isAddressPresent', function(req, res, next) {	
+	
+	var users = db.get().collection('users');
+	var address = req.get("address");
+	users.findOne({ address: address}, function (err, user) {
+		if (err) {
+			return res.status(200).json({
+				status: 'error',
+				error: 'Unable to find address in db'
+			});
+		}
+		
+		if (user !== null) {
+			return res.status(200).json({
+				status: 'error',
+				error: 'Address already exist'
+			});
+		}
+
+		return res.status(200).json({
+			status: 'error',
+			error: 'Address not exist'
+		});
+	});
+});
+
+router.get('/getUserDetails', function(req, res, next) {	
+	
+	var users = db.get().collection('users');
+	var address = req.get("address");
+	users.findOne({ address: address}, function (err, user) {
+		if (err || user == null) {
+			return res.status(200).json({
+				status: 'error',
+				error: 'Unable to find address in db',
+				user: {}
+			});
+		}
+		
+		return res.status(200).json({
+			status: 'ok',
+			error: '',
+			user: user
+		});
 	});
 });
 
