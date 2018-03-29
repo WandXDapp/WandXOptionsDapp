210570734af40bb68f2b149dd57620c4f978f322
Nishant Bhatia <nishant@roshi.in>
2018-03-29 13:56:00 +0530

    added update profile hit

--- routes/index.js
+++ routes/index.js
@@ -21,10 +21,10 @@ Method: GET
 Purpose: This is the home page of the website. Before login. If user is already authenticated
 		 then user is redirected to the exchange page.
 **/
-router.get('/insertAddress', function(req, res, next) {	
+router.put('/insertAddress', function(req, res, next) {	
 	
 	var users = db.get().collection('users');
-	var address = req.get("address");
+	var address = req.body.address;
 	users.findOne({ address: address}, function (err, user) {
 		if (err) {
 			return res.status(200).json({
@@ -104,6 +104,55 @@ router.get('/getUserDetails', function(req, res, next) {
 			status: 'ok',
 			error: '',
 			user: user
+		});
+	});
+});
+
+router.put('/updateUserProfile', function(req, res, next) {	
+
+	var users = db.get().collection('users');
+	
+	console.log(req.body);
+	
+	var address = req.body.address;
+	var profile = {
+		company: req.body.company,
+		username: req.body.username,
+		email: req.body.email,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		residential_address: req.body.residential_address,
+		city: req.body.city,
+		country: req.body.country,
+		pincode: req.body.pincode,
+		aboutMe: req.body.aboutMe
+	}
+	
+	users.findOne({ address: address}, function (err, user) {
+		if (err || user == null) {
+			return res.status(200).json({
+				status: 'Unable to find address in db',
+				error: err,
+				result: false
+			});
+		}
+		
+		var myquery = { address: address };
+		var newvalues = { $set: profile };
+		users.update(myquery, newvalues, function(err, res1) {
+			if (err){
+				return res.status(200).json({
+					status: 'Unable to update user',
+					error: err,
+					result: false
+				});
+			}
+
+			return res.status(200).json({
+				status: 'ok',
+				error: '',
+				result: true
+			});
 		});
 	});
 });
